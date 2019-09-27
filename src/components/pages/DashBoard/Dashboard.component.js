e41053e71e11e2ccfb5beaf4f533e82b08de820b
Daniel Herrero <danielherrerohernando@gmail.com>
2019-09-27 14:37:13 +0200

    adding slack integration

--- src/components/pages/DashBoard/Dashboard.component.js
+++ src/components/pages/DashBoard/Dashboard.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Graph from './BarChart.component';
+import DashHome from './DashHome';
 import './graph.css';
 
 
@@ -7,6 +8,7 @@ const renderUsers = users => users && users.map(user => <div key={user.id}>{user
 
 const Dashboard = ({ user: { project } }) => (
   <div>
+    {!project && <DashHome />}
     {project && renderUsers(project.users)}
     <div className="graph">
       {project && <Graph />}
