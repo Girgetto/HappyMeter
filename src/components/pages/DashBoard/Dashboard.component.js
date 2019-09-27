6f8424e54ec9d07b2ae038ff1d1b778e1bccc139
Girgetto <giorgiograssini@gmail.com>
2019-09-27 14:32:06 +0200

    Random in graph

--- src/components/pages/DashBoard/Dashboard.component.js
+++ src/components/pages/DashBoard/Dashboard.component.js
@@ -2,14 +2,10 @@ import React from 'react';
 import Graph from './BarChart.component';
 import './graph.css';
 
-
-const renderUsers = users => users && users.map(user => <div key={user.id}>{user.name} {user.value}</div>);
-
 const Dashboard = ({ user: { project } }) => (
   <div>
-    {project && renderUsers(project.users)}
     <div className="graph">
-      <Graph />
+      {project && <Graph users={project.users} />}
     </div>
 
   </div>
