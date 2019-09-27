588303b57563551dc9f4b4655125fe105907af48
Girgetto <giorgiograssini@gmail.com>
2019-09-27 09:43:58 +0200

    Fixed login

--- src/components/pages/Login/Login.component.js
+++ src/components/pages/Login/Login.component.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const Login = () => {
-  return (
-    <div>
+const Login = ({ className }) => (
+  <div className={className}>
       Login
-    </div>
-  )
-}
+  </div>
+);
 
-export default Login
+export default Login;
